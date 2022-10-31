3382b699ef5e73abcead1ac1b929c0b9aa1a3e0a
Isaac Pontes <isaaclfp@outlook.com>
2022-10-31 17:08:02 -0300

    feat: add dotenv and database integration.

--- src/index.js
+++ src/index.js
@@ -1,4 +1,18 @@
+import dotenv from "dotenv";
 import express from "express";
+import { DataTypes, Sequelize } from "sequelize";
+
+dotenv.config()
+
+const sequelize = new Sequelize(process.env.MYSQL_URL)
+
+const User = sequelize.define("User", {
+  id: { type: DataTypes.INTEGER, allowNull: false, autoIncrement: true, primaryKey: true },
+  name: { type: DataTypes.STRING, allowNull: false }
+}, {
+  tableName: "users",
+  timestamps: false
+})
 
 const app = express()
 
@@ -9,6 +23,25 @@ app.get("/", (req, res) => {
     })
   } catch (err) {
     res.status(400).json({ message: err.message })
+  }
+})
+
+app.post("/users", async (req, res) => {
+  try {
+    const { name } = req.query
+    const user = await User.create({ name })
+    return res.status(201).json(user)
+  } catch (err) {
+    return res.status(400).json({ message: err.message })
+  }
+})
+
+app.get("/users", async (req, res) => {
+  try {
+    const users = await User.findAll()
+    return res.json(users)
+  } catch (err) {
+    return res.status(400).json({ message: err.message })
   }
 })
 
